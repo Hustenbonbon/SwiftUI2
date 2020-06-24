42f3d5ee6812ae5cdd2f38d89f33294312175e6b
Max Tharr <max.tharr@mayflower.de>
2020-06-24 21:33:56 +0200

    Initial Commit

---
+++ rumspielprojekt/rumspielprojektApp.swift
@@ -0,0 +1,17 @@
+//
+//  rumspielprojektApp.swift
+//  rumspielprojekt
+//
+//  Created by Max Tharr on 24.06.20.
+//
+
+import SwiftUI
+
+@main
+struct rumspielprojektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
